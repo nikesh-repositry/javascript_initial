7d5af0cc033f66b990031dbefff2bf3e69a357b7
NIKESH <nikeshnoida@gmail.com>
2025-02-28 23:38:07 +0530

    Add files via upload

---
+++ 3identifier.js
@@ -0,0 +1,19 @@
+/*JavaScript Identifiers
+All JavaScript variables must be identified with unique names.
+
+These unique names are called identifiers.
+
+Identifiers can be short names (like x and y) or more descriptive names (age, sum, totalVolume).
+
+The general rules for constructing names for variables (unique identifiers) are:
+
+Names can contain letters, digits, underscores, and dollar signs.
+Names must begin with a letter.
+Names can also begin with $ and _ .
+Names are case sensitive (y and Y are different variables).
+Reserved words (like JavaScript keywords) cannot be used as names.*/
+console.log(x);
+var x=3;
+console.log("--------------------------------------------")
+console.log(y);
+let y=4
