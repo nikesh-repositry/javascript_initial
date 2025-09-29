efe55bcf75679b43d3d948c32c6fc08ef7e2a431
NIKESH <nikeshnoida@gmail.com>
2025-09-29 23:47:39 +0530

    Update 14event.js

--- 14eventHandling/14event.js
+++ 14eventHandling/14event.js
@@ -1,1 +1,7 @@
-console.log("event handling========================================")
+console.log("EVENT HANDLING++++++++++++++++++++++++++++++++++")
+function fun1(){
+    alert("hey i am a alert box ")
+}
+function fun2(){
+    document.body.classList.toggle('yellowBG')
+}
