3c51843c466e8730bb459215185730eca0c9a0e6
NIKESH <nikeshnoida@gmail.com>
2025-10-03 23:01:34 +0530

    Update 14event.js

--- 14eventHandling/14event.js
+++ 14eventHandling/14event.js
@@ -5,3 +5,21 @@ function fun1(){
 function fun2(){
     document.body.classList.toggle('yellowBG')
 }
+
+const a1 = document.getElementById("mydiv1")
+a1.onmouseover=()=>{
+    a1.style.backgroundColor = "aqua"
+}
+a1.onmouseout=()=>{
+    a1.style.backgroundColor = "fuchsia"
+}
+
+const a2 = document.getElementById('mydiv2')
+const a3 = document.getElementById('but1')
+const a4 = document.getElementById('but2')
+a3.addEventListener('click',function(){
+    a2.style.backgroundColor = "yellow"
+})
+a4.addEventListener('click',()=>{
+    a2.style.backgroundColor = "white"
+})
