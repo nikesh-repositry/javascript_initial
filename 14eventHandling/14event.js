6b04b5cded6693bf8d51f364d6359893b0d1bd70
NIKESH <nikeshnoida@gmail.com>
2025-10-03 23:14:34 +0530

    Update 14event.js

--- 14eventHandling/14event.js
+++ 14eventHandling/14event.js
@@ -1,11 +1,13 @@
 console.log("EVENT HANDLING++++++++++++++++++++++++++++++++++")
+//inline event handling 
 function fun1(){
     alert("hey i am a alert box ")
 }
 function fun2(){
-    document.body.classList.toggle('yellowBG')
+    document.body.classList.toggle('yellowBG') //here css selection through class name and then toggle method which used to active if inactive and inactive if active
 }
 
+//event handling using dom event property
 const a1 = document.getElementById("mydiv1")
 a1.onmouseover=()=>{
     a1.style.backgroundColor = "aqua"
@@ -14,6 +16,7 @@ a1.onmouseout=()=>{
     a1.style.backgroundColor = "fuchsia"
 }
 
+//event hagdling using addEventListener
 const a2 = document.getElementById('mydiv2')
 const a3 = document.getElementById('but1')
 const a4 = document.getElementById('but2')
